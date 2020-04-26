f1031d3f16f843163dcc4296f7ac28beb1283f27
Roman-Moiseenko <60714951+Roman-Moiseenko@users.noreply.github.com>
2020-04-27 01:26:44 +1200

    Category Characteristic Tag => Controller. Before Edit

---
+++ backend/views/shop/category/_form.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model shop\entities\shop\Category */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="category-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'id')->textInput() ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'slug')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'meta_json')->textInput() ?>
+
+    <?= $form->field($model, 'lft')->textInput() ?>
+
+    <?= $form->field($model, 'rgt')->textInput() ?>
+
+    <?= $form->field($model, 'depth')->textInput() ?>
+
+    <?= $form->field($model, 'code1C')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'id_old')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
