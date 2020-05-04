567aa1c094dfa8da1e1b8fbc33bf37ba5f4e5a06
Roman-Moiseenko <60714951+Roman-Moiseenko@users.noreply.github.com>
2020-05-04 22:36:19 +1200

    js

---
+++ backend/web/js/site.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $('body').on('click', '.featured-check', function () {
+        let id = $(this).attr('data-id');
+        let value = $(this).attr('value');
+        $.post("/shop/product/featured?id="+id+"&featured="+value, {id: id, featured: value}, function (data) {
+        });
+    });
+
+})(window.jQuery);
